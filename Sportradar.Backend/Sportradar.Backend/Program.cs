4bd4e8660d0fb25f21efc4019d44b056e209079d
BanhBaoz <q.vuvan05@gmail.com>
2026-03-25 17:44:59 +0100

    fix: add database autocreation for docker instances

--- Sportradar.Backend/Sportradar.Backend/Program.cs
+++ Sportradar.Backend/Sportradar.Backend/Program.cs
@@ -26,6 +26,9 @@ public class Program
         using (var scope = app.Services.CreateScope())
         {
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await context.Database.MigrateAsync();
+
             await DbSeed.SeedAsync(context);
         }
 
